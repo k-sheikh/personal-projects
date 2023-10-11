f5df8fc09f474651cb3a2302f09f895bab55ef9e
Kashan Sheikh <k-sheikh@hotmail.com>
2023-10-11 23:55:53 +0100

    Modify password validation to include alphanumeric

--- Banking Environment/banking_app.py
+++ Banking Environment/banking_app.py
@@ -40,7 +40,11 @@ class Bank:
     @staticmethod
     def validate_password(password):
         # Ensure password is >= 8 chars and contains a number
-        return len(password) >= 8 and any(char.isdigit() for char in password)
+        return (len(password) >= 8 and
+                any(char.isdigit() for char in password) and
+                any(char.isalpha() for char in password) and
+                any(char.islower() for char in password) and
+                any(char.isupper() for char in password))
 
 
 # Child class
