3593431e93e48c00d4ce9118a4fa98b69dab8b62
Kashan Sheikh <k-sheikh@hotmail.com>
2023-12-14 15:53:42 +0000

    Write pseudocode

--- Banking Environment/banking_app.py
+++ Banking Environment/banking_app.py
@@ -2,6 +2,24 @@
 
 # -----PSEUDOCODE-----
 
+# Main Menu
+# 	Welcome, Login, Register, Exit
+# 		Login
+# 			Enter user id, Enter password
+# 				You have no accounts, Create account
+# 					Account type: Savings, Current
+# 					Save Classes to txt
+# 				Select account
+# 					Check balance, Withdraw, Deposit
+# 						Deposit with cash, transfer from another account
+# 				Create new account
+# 					Account type: Savings, Current
+# 					Save Classes to txt
+# 		Register
+# 			Create user id, password
+# 			Verify user id, password
+# 			Check for match, save to txt
+
 # -----LIBRARIES-----
 
 # -----CLASSES-----
