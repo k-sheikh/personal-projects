8bee766ce6bcdffd36c1d7782c5e64a4128fd840
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-08 23:25:23 +0000

    Rewrite function logic to generate account number

--- Banking Environment/banking_app.py
+++ Banking Environment/banking_app.py
@@ -101,7 +101,10 @@ class Bank:
 
 def generate_user_id(forename, surname):
     return f"{forename[0].lower()}{surname[:5].lower()}{random.randint(1000, 9999)}"
-    
+
+
+def generate_account_number():
+    return random.randint(10000000, 99999999)
 
 
 # -----FUNCTIONS-----
