b70830296ff7fcf779cdedeecf63931615ee6155
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-09 23:00:37 +0000

    Refactor: Move utility functions from main to utilities.py

--- Banking Environment/banking_app.py
+++ Banking Environment/banking_app.py
@@ -49,26 +49,6 @@ import stdiomask
 users = {}
 
 
-# -----CLASSES-----
-
-
-
-
-
-        
-
-
-
-# -----UTILITY FUNCTIONS-----
-
-def generate_user_id(forename, surname):
-    return f"{forename[0].lower()}{surname[:5].lower()}{random.randint(1000, 9999)}"
-
-
-def generate_account_number():
-    return random.randint(10000000, 99999999)
-
-
 # -----FUNCTIONS-----
 
 # Welcome message
---
+++ Banking Environment/utilities.py
@@ -0,0 +1,8 @@
+# -----UTILITY FUNCTIONS-----
+
+def generate_user_id(forename, surname):
+    return f"{forename[0].lower()}{surname[:5].lower()}{random.randint(1000, 9999)}"
+
+
+def generate_account_number():
+    return random.randint(10000000, 99999999)
