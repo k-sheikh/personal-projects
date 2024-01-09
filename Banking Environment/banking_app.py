caf5a0c83d62aac3697f2f1185f4c34556de60bb
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-09 22:56:44 +0000

    Refactor: Move Account class from main to account.py

--- Banking Environment/banking_app.py
+++ Banking Environment/banking_app.py
@@ -54,22 +54,7 @@ users = {}
 
 
 
-class Account:
-    def __init__(self, account_number, account_type, balance = 0):
-        self.account_number = account_number
-        self.account_type = account_type
-        self.balance = balance
 
-    def deposit(self, amount):
-        self.balance += amount
-        return self.balance
-    
-    def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            return self.balance
-        else:
-            raise ValueError("Insufficient funds")
         
 
 class Bank:
