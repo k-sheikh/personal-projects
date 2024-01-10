b04356ccb5edc3612fe7757bf9a1a10fb997268b
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-10 00:40:48 +0000

    Update user.py to  align with database structure

--- Banking Environment/user.py
+++ Banking Environment/user.py
@@ -6,5 +6,10 @@ class User:
         self.password = password
         self.accounts = {}
 
-    def add_account(self, accounts):
-        self.accounts[accounts.account_number] = accounts
+    def add_account(self, account):
+        # Add an account to the user
+        self.accounts[account.account_number] = account
+
+    def get_account(self, account_number):
+        # Retrieve a specific account
+        return self.accounts.get(account_number)
