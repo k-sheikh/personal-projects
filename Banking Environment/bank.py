189145c0f0dd6e2a7335a45feae85da77fb8c62f
Kashan Sheikh <k-sheikh@hotmail.com>
2024-01-10 00:32:18 +0000

    Update bank.py to interact with SQLite database

--- Banking Environment/bank.py
+++ Banking Environment/bank.py
@@ -1,29 +1,35 @@
 from user import User
 from account import Account
 from utilities import generate_user_id, generate_account_number
+import sqlite3
+from database import create_connection
 
 class Bank:
     def __init__(self):
-        self.users = {}
+        self.conn = create_connection()
 
     def register_user(self, forename, surname, password):
+        user_id = generate_user_id(forename, surname)
         try:
-            user_id = generate_user_id(forename, surname)
-            if user_id in self.users:
-                raise ValueError("User already exists")
-            new_user = User(forename, surname, user_id, password)
-            self.users[user_id] = new_user
-            return new_user
+            c = self.conn.cursor()
+            c.execute("INSERT INTO users (user_id, forename, surname, password) VALUES (?, ?, ?, ?)",
+                      (user_id, forename, surname, password))
+            self.conn.commit()
+        except sqlite3.IntegrityError:
+            raise ValueError("User ID already exists")
         except Exception as e:
-            print(f"Error in user registration: {e}")
+            raise e
+        return User(forename, surname, user_id, password)
     
     def create_account(self, user_id, account_type):
-        try:
-            if user_id not in self.users:
-                raise ValueError("User does not exist")
             account_number = generate_account_number()
-            account = Account(account_number, account_type)
-            self.users[user_id].add_account(account)
-            return account
-        except Exception as e:
-            raise Exception(f"Error in account creation: {e}")
+            try:
+                c = self.conn.cursor()
+                c.execute("INSERT INTO accounts (account_number, user_id, account_type, balance) VALUES (?, ?, ?, ?)",
+                        (account_number, user_id,account_type, 0))
+                self.conn.commit()
+            except sqlite3.IntegrityError:
+                 raise ValueError("Account number already exists")
+            except Exception as e:
+                 raise e
+            return Account(account_number, account_type)
